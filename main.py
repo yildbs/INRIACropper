cb012454f9501e644bd0d6a5e967c9e2e89d2df2
Illo Yoon <yildbs@gmail.com>
2017-09-22 22:59:12 +0900

    * update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import glob
 import cv2
 import codecs
+import os
 
 
 def get_part_image(part, rect, image, offset=0):
@@ -19,21 +20,28 @@ def get_part_image(part, rect, image, offset=0):
     rect_width = org_x2 - org_x1
     rect_height = org_y2 - org_y1
 
-    relative_position_of_part_x1 = relative_position_of_part[part][0] * (1.0 + offset_x1)
-    relative_position_of_part_y1 = relative_position_of_part[part][1] * (1.0 + offset_y1)
-    relative_position_of_part_x2 = relative_position_of_part[part][2] * (1.0 + offset_x2)
-    relative_position_of_part_y2 = relative_position_of_part[part][3] * (1.0 + offset_y2)
+    relative_position_of_part_x1 = relative_position_of_part[part][0]
+    relative_position_of_part_y1 = relative_position_of_part[part][1]
+    relative_position_of_part_x2 = relative_position_of_part[part][2]
+    relative_position_of_part_y2 = relative_position_of_part[part][3]
 
     x1 = org_x1 + rect_width * relative_position_of_part_x1
     y1 = org_y1 + rect_height * relative_position_of_part_y1
     x2 = org_x1 + rect_width * relative_position_of_part_x2
     y2 = org_y1 + rect_height * relative_position_of_part_y2
 
-    center_x = (x1 + x2) / 2
-    center_y = (y1 + y2) / 2
+    rect_width = x2 - x1
+    rect_height = y2 - y1
+
+    x1 += rect_width * offset[0]
+    y1 += rect_height * offset[1]
+    x2 += rect_width * offset[2]
+    y2 += rect_height * offset[3]
 
     rect_width = x2 - x1
     rect_height = y2 - y1
+    center_x = (x1 + x2) / 2
+    center_y = (y1 + y2) / 2
 
     image_width = image.shape[1]
     image_height = image.shape[0]
@@ -67,29 +75,10 @@ def get_part_image(part, rect, image, offset=0):
     cropped = image[y1:y2, x1:x2, :]
     return cropped
 
-
-if __name__ == "__main__":
-    # Absolute path recommended
-    annotations_path = '/home/yildbs/Data/INRIA/Train_original/annotations/'
-    images_path = '/home/yildbs/Data/INRIA/Train_original/pos/'
-    save_path = './output_train/'
-
-    # annotations_path = '/home/yildbs/Data/INRIA/Test_original/annotations/'
-    # images_path = '/home/yildbs/Data/INRIA/Test_original/pos/'
-    # save_path = './output_test/'
-
+def make_db(annotations_path, images_path, save_path):
     image_name_list = glob.glob(images_path+'/*.png')
-
-    fit_to_ratio = True
-    h_over_w = 1.0
-    margin_pixel = 0
-    margin_ratio = 1.0
-
     for image_name in image_name_list:
         image_name = image_name[image_name.rfind('/')+1:image_name.rfind('.')]
-        image_width = 0
-        image_height = 0
-        image_channels = 0
         rects = []
         with codecs.open(annotations_path+'/'+image_name+'.txt', 'r', "ISO-8859-1") as f:
             for line in f:
@@ -110,7 +99,7 @@ if __name__ == "__main__":
             image = cv2.imread(images_path + '/' + image_name + '.png')
 
             try:
-                cv2.destroyAllWindows()
+                # cv2.destroyAllWindows()
                 for idx, rect in zip(range(9999), rects):
                     parts = ['head', 'upper_body', 'lower_body', 'full_body', 'full_body_head']
                     offsets = {}
@@ -118,27 +107,47 @@ if __name__ == "__main__":
                     offsets['broad'] = [-0.1, -0.1, 0.1, 0.1]
                     offsets['left'] = [-0.1, 0., -0.1, 0.]
                     offsets['right'] = [0.1, 0., 0.1, 0.]
-                    offsets['top'] = [0., -0.1, 0., -0.1]
-                    offsets['bot'] = [0., 0.1, 0., 0.1]
-                    offsets['left_top'] = [-0.1, -0.1, -0.1, -0.1]
-                    offsets['right_top'] = [0.1, -0.1, 0.1, -0.1]
-                    offsets['left_bot'] = [-0.1, 0.1, -0.1, 0.1]
-                    offsets['right_bot'] = [0.1, 0.1, 0.1, 0.1]
-
+                    offsets['top'] = [0., -0.05, 0., -0.05]
+                    offsets['bot'] = [0., 0.05, 0., 0.05]
+                    offsets['left_top'] = [-0.1, -0.05, -0.1, -0.05]
+                    offsets['right_top'] = [0.1, -0.05, 0.1, -0.05]
+                    offsets['left_bot'] = [-0.1, 0.05, -0.1, 0.05]
+                    offsets['right_bot'] = [0.1, 0.05, 0.1, 0.05]
                     images = []
                     try:
                         for part in parts:
-                            cropped = get_part_image(part, rect, image, offsets['left_top'])
-                            # images.append([part, cropped])
-                            cv2.imshow(part, cropped)
-                    # save_filename = save_path + image_name + '_cropped_' + '%06d' % idx + '.jpg'
-                    # cv2.imwrite(save_filename, cropped)
-                    # print('Cropped image is saved at ', save_filename)
+                            for offset in offsets.keys():
+                                cropped = get_part_image(part, rect, image, offsets[offset])
+                                # cv2.imshow(part, cropped)
+                                # cv2.waitKey(1)
+                                save_filename = save_path + '/' + part + '/' + 'cropped_' + image_name + '_' + offset + '.jpg'
+                                try:
+                                    os.makedirs(save_filename[:save_filename.rfind('/')])
+                                except:
+                                    pass
+                                cv2.imwrite(save_filename, cropped)
+                                # print('Cropped image is saved at ', save_filename)
                     except:
                         pass
 
-                    cv2.waitKey(0)
+                    # cv2.waitKey(0)
 
             except:
                 pass
 
+
+
+if __name__ == "__main__":
+
+    print('Current working directory : ', os.getcwd())
+
+    # Absolute path recommended
+    annotations_path = '/home/yildbs/Data/INRIA/Train_original/annotations/'
+    images_path = '/home/yildbs/Data/INRIA/Train_original/pos/'
+    save_path = './output_train/'
+    make_db(annotations_path, images_path, save_path)
+
+    annotations_path = '/home/yildbs/Data/INRIA/Test_original/annotations/'
+    images_path = '/home/yildbs/Data/INRIA/Test_original/pos/'
+    save_path = './output_test/'
+    make_db(annotations_path, images_path, save_path)
